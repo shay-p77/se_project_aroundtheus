d99192b9dbe791beb0c433a952639d0f276f074c
shay-p <shaypaley@gmail.com>
2025-01-04 22:29:44 +0200

    final stage of sprint 4

--- scripts/index.js
+++ scripts/index.js
@@ -30,21 +30,58 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+// Elements
 
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileEditModalClose = document.querySelector("#profile-modal-close");
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileSubtitleInput = document.querySelector("#profile-subtitle-input");
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+
+// functions
+
+function closePopup() {
+  profileEditModal.classList.remove("modal__open");
+}
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement
+    .querySelector(".card__image")
+    .setAttribute("src", cardData.link);
+  const cardTitleEl = cardElement.querySelector(".card__title");
+
+  cardTitleEl.textContent = cardData.name;
+
+  return cardElement;
+}
+
+// eventlisteners
 
 profileEditButton.addEventListener("click", () => {
-  profileEditModal.classList.add('modal__open');
+  profileTitleInput.value = profileTitle.textContent;
+  profileSubtitleInput.value = profileSubtitle.textContent;
+  profileEditModal.classList.add("modal__open");
 });
-
- 
 
 profileEditModalClose.addEventListener("click", () => {
-  profileEditModalClose.classList.remove('modal__open');
+  closePopup();
 });
 
-// get the modal close button to work
- 
+profileEditForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileSubtitle.textContent = profileSubtitleInput.value;
+  closePopup();
+});
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.append(cardElement);
+});
