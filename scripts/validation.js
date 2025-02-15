335535e52542f6a4004be57488e7968662888efc
shay-p <shaypaley@gmail.com>
2025-02-15 15:19:22 +0200

    fixed button inactive state

--- scripts/validation.js
+++ scripts/validation.js
@@ -43,10 +43,10 @@ function setEventListeners(formEl, options) {
   const { inputSelector } = options;
   const inputEls = [...formEl.querySelectorAll(inputSelector)];
   const submitButton = formEl.querySelector(options.submitButtonSelector);
+  toggleButtonState(inputEls, submitButton, options);
   inputEls.forEach((inputEl) => {
     inputEl.addEventListener("input", (e) => {
       checkInputValidity(formEl, inputEl, options);
-      toggleButtonState(inputEls, submitButton, options);
     });
   });
 }
